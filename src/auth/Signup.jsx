3ab96606f186785b1fa7b6a852ab949d100a6a77
Sabrina Haque <sabrina.haque442@gmail.com>
2025-05-22 23:36:10 +0600

    implemented firebase signup with email, password

--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -1,10 +1,11 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import React, { useState } from 'react';
 import { FcGoogle } from 'react-icons/fc';
 import { NavLink, useLocation, useNavigate } from 'react-router';
 import { auth } from '../firebase/firebase.init';
 
 const Signup = () => {
+    const [errorMessage, setErrorMessage]=useState('')
     const navigate = useNavigate()
     const location= useLocation()
     const from = location.state?.from || '/'
@@ -23,11 +24,31 @@ const Signup = () => {
     })
     }
 
+    const handleSignup = e =>{
+        e.preventDefault()
+        const name = e.target.name.value
+        const email = e.target.email.value
+        const password = e.target.password.value
+        const photoURL = e.target.photoURL.value
+        console.log(name, email, password, photoURL)
+
+        setErrorMessage('')
+        // create user firebase
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(result =>{
+            console.log(result)
+        })
+        .catch(error =>{
+            console.log(error)
+            setErrorMessage(error.message)
+        })
+    }
+
     return (
         <div className="card bg-base-100 max-w-sm mx-auto shrink-0 mt-1">
             <div className="card-body">
             <h1 className="text-3xl font-bold text-green-700">Signup!</h1>
-            <form className="fieldset">
+            <form onSubmit={handleSignup} className="fieldset">
                 <label className="label">Name</label>
                 <input type="text" name="name" className="input" placeholder="Name" />
 
@@ -35,14 +56,18 @@ const Signup = () => {
                 <input type="email" name="email" className="input" placeholder="Email" />
 
                 <label className="label">Photo URL</label>
-                <input type="text" name="photo" className="input" placeholder="Photo URL" />
+                <input type="text" name="photoURL" className="input" placeholder="Photo URL" />
 
                 <label className="label">Password</label>
                 <input type="password" name="password" className="input" placeholder="Password" />
                 
                 <button className="btn rounded-full bg-green-700 text-white mt-4">Signup</button>
+                {
+                    errorMessage && <p className='text-red-600'>{errorMessage}</p>
+                }
                 <p className='text-lg'>Already have an account? <NavLink to='/login' className='text-blue-600 font-bold hover:underline'>Login</NavLink>.</p>
             </form>
+            
             <p className='text-center text-gray-500 text-lg'>-- OR --</p>
             <button onClick={handleGoogleSignin} className="btn rounded-full border-green-700"><FcGoogle size={20} />Signin with Google</button>
             </div>
