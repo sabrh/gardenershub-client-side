aef242048c204ed2623b55ec32d83ba064256f96
Sabrina Haque <sabrina.haque442@gmail.com>
2025-05-24 21:22:43 +0600

    added challenge difficulty filter in Browse Tips page

--- src/pages/BrowseTips.jsx
+++ src/pages/BrowseTips.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {useLoaderData} from 'react-router';
 import SharedTipCard from '../components/SharedTipCard';
 import { FaGlobe } from 'react-icons/fa';
@@ -7,12 +7,27 @@ const BrowseTips = () => {
     const tips=useLoaderData()
     console.log(tips)
 
+    const [difficultyFilter, setDifficultyFilter]=useState('')
+
+    const filteredTips=difficultyFilter ? tips.filter(tip=>tip.difficulty === difficultyFilter)
+    : tips;
+
     return (
         <div className='my-1'>
             <p className='flex items-center mb-3 text-gray-500'>Tips Status: <FaGlobe /> Public </p>
+
+        <div className='mb-4'>
+            <select onChange={(e) => setDifficultyFilter(e.target.value)} className="select select-bordered w-full max-w-xs" value={difficultyFilter}>
+                <option value="">All Difficulty Levels</option>
+                <option value="Easy">Easy</option>
+                <option value="Medium">Medium</option>
+                <option value="Hard">Hard</option>
+            </select>
+        </div>
+
             <div className='grid grid-cols-1 gap-6'>
             {
-                tips.map(tip => <SharedTipCard key={tip._id} tip={tip}></SharedTipCard>)
+                filteredTips.map(tip => <SharedTipCard key={tip._id} tip={tip}></SharedTipCard>)
             }
             </div>
         </div>
