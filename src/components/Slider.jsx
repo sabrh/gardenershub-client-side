b646378b8e5468af2f06438e31402a5415d8c692
Sabrina Haque <sabrina.haque442@gmail.com>
2025-05-24 22:52:44 +0600

    tract-awesome-reveal in homepage banner

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Fade } from "react-awesome-reveal";
 
 const Slider = () => {
     return (
@@ -9,10 +10,13 @@ const Slider = () => {
             className="w-full h-full bg-cover bg-center flex items-center"
             style={{ backgroundImage: "url('https://grangettos.com/cdn/shop/files/events-header_1600x.jpg?v=1614309146')" }}
             >
+            
             <div className="bg-white bg-opacity-50 p-10 text-black max-w-md m-4 md:m-0 md:ml-10 rounded">
+                <Fade>
                 <h2 className="text-3xl font-bold mb-2">Garden Workshop</h2>
                 <p className="mb-4">Learn how to plant and grow herbs at home with expert tips.</p>
                 <button className="btn btn-success">Join Event</button>
+                </Fade>
             </div>
             </div>
             <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
@@ -27,9 +31,11 @@ const Slider = () => {
             style={{ backgroundImage: "url('https://sajam.rs/wp-content/uploads/horti2016_7954.jpg')" }}
             >
             <div className="bg-white bg-opacity-50 p-10 text-black max-w-md m-4 md:m-0 md:ml-10 rounded">
+                <Fade>
                 <h2 className="text-3xl font-bold mb-2">Flower Arrangement Fair</h2>
                 <p className="mb-4">Join us for a creative afternoon of flower arranging and refreshments.</p>
                 <button className="btn btn-success">Register Now</button>
+                </Fade>
             </div>
             </div>
             <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
@@ -44,9 +50,11 @@ const Slider = () => {
             style={{ backgroundImage: "url('https://mb.cision.com/Public/5132/2079562/87358215c70008a8_800x800ar.jpg')" }}
             >
             <div className="bg-white bg-opacity-50 p-10 text-black max-w-md m-4 md:m-0 md:ml-10 rounded">
+                <Fade>
                 <h2 className="text-3xl font-bold mb-2">Organic Food Fest</h2>
                 <p className="mb-4">Taste fresh produce and meet local gardeners and farmers.</p>
                 <button className="btn btn-success">See Details</button>
+                </Fade>
             </div>
             </div>
             <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
